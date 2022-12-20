4664a715bd91849963ed4b3da8d4803dbcfaafa0
Roman Troshin <roman.troshin98@gmail.com>
2022-12-20 16:56:29 +0300

    feat: add error boundary

---
+++ components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import { Component, ReactNode } from 'react';
+import Router from 'next/router';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    const { children } = this.props;
+    const { hasError } = this.state;
+
+    if (hasError) {
+      Router.push('/404');
+      return null;
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/system';
 
+import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
+
 import store from 'redux/store';
 import defaultTheme from 'styles/theme';
 import GlobalStyle from 'styles/Global';
@@ -10,8 +12,10 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <ThemeProvider theme={defaultTheme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
+        <ErrorBoundary>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ErrorBoundary>
       </ThemeProvider>
     </Provider>
   );
