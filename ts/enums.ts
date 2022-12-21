f3edb445ad3641cb52f40cac69abc0b5e7ca2f4f
Roman Troshin <roman.troshin98@gmail.com>
2022-12-22 01:23:37 +0300

    feat: add endpoints and methods

--- ts/enums.ts
+++ ts/enums.ts
@@ -1,1 +1,23 @@
-export type {};
+enum Endpoints {
+  topRated = 'movie/top_rated',
+  latest = 'movie/now_playing',
+  lists = 'movie/lists',
+  popular = 'movie/popular',
+  upcoming = 'movie/upcoming',
+  searchMovie = 'search/movie',
+  searchPerson = 'search/person',
+  searchTv = 'search/tv',
+  searchCollections = 'search/collection',
+  searchCompanies = 'search/company',
+  multiSearch = 'search/multi',
+}
+
+enum Methods {
+  get = 'GET',
+  post = 'POST',
+  patch = 'PATCH',
+  put = 'PUT',
+  delete = 'DELETE',
+}
+
+export { Endpoints, Methods };
