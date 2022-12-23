24418227428f22647d0c80a685a13e2529d7db05
Roman Troshin <roman.troshin98@gmail.com>
2022-12-23 21:36:23 +0300

    fix: update home test file

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -7,11 +7,13 @@ import defaultTheme from 'styles/theme';
 
 import Home from 'pages/index';
 
+import mockedMovies from './utils/constants';
+
 const setUp = () =>
   render(
     <Provider store={store}>
       <ThemeProvider theme={defaultTheme}>
-        <Home moviePosters={[]} />
+        <Home moviePosters={mockedMovies} />
       </ThemeProvider>
     </Provider>
   );
@@ -24,5 +26,6 @@ describe('Home page', () => {
   test('should render Home page', () => {
     expect(screen.getByRole('heading', { name: /movie app/i })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /enter/i })).toBeInTheDocument();
+    expect(screen.getAllByRole('img')).toHaveLength(2);
   });
 });
---
+++ __tests__/utils/constants.ts
@@ -0,0 +1,40 @@
+import { Movie } from 'ts/interfaces';
+
+const mockedMovies: Movie[][] = [
+  [
+    {
+      id: 1,
+      adult: false,
+      backdrop_path: '/cxvx',
+      genre_ids: [],
+      original_language: 'dsaxc',
+      original_title: 'first',
+      title: 'first',
+      popularity: 5,
+      overview: 'vxcxcv',
+      poster_path: '/xvcx',
+      video: false,
+      release_date: 'sxcs',
+      vote_average: 2,
+      vote_count: 6,
+    },
+    {
+      id: 2,
+      adult: false,
+      backdrop_path: '/cxvx',
+      genre_ids: [],
+      original_language: 'dsaxc',
+      original_title: 'second',
+      title: 'second',
+      popularity: 5,
+      overview: 'vxcxcv',
+      poster_path: '/xvcx',
+      video: false,
+      release_date: 'sxcs',
+      vote_average: 2,
+      vote_count: 6,
+    },
+  ],
+];
+
+export default mockedMovies;
