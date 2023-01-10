ebc968bc92b484723334e812d46b31e49c4d33d1
Roman Troshin <roman.troshin98@gmail.com>
2023-01-10 10:52:06 +0300

    fix: fix pagination styles

--- components/CardsControl/CardsControl.style.tsx
+++ components/CardsControl/CardsControl.style.tsx
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles';
 
 const StyledGridContainer = styled(Grid)({
   justifyContent: 'center',
+  padding: '0 1rem',
 });
 
 const StyledPagination = styled(Pagination)(({ theme }) => ({
@@ -10,8 +11,21 @@ const StyledPagination = styled(Pagination)(({ theme }) => ({
     color: theme.palette.background.white,
   },
 
+  '& .MuiPagination-ul': {
+    gap: '0.38rem 0',
+  },
+
   '& Button': {
-    border: '1px solid rgba(255, 255, 255, 0.23)',
+    border: '0.06rem solid rgba(255, 255, 255, 0.23)',
+    minWidth: '2rem',
+    height: '2rem',
+    borderRadius: '0.25rem',
+    margin: '0 0.19rem',
+    padding: '0 0.38rem',
+
+    '& .Mui-selected': {
+      color: theme.palette.text.primary,
+    },
 
     '&:hover': {
       backgroundColor: 'rgba(255, 255, 255, 0.1)',
