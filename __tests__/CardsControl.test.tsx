0072811ca0cd5e42ff5ba0ef46ac32eed9b559b5
Roman Troshin <roman.troshin98@gmail.com>
2023-01-28 15:17:01 +0300

    fix: fix cardscontrol tests

--- __tests__/CardsControl.test.tsx
+++ __tests__/CardsControl.test.tsx
@@ -7,10 +7,12 @@ import CardsControl from 'components/CardsControl/CardsControl';
 
 import mockNextRouter from './test-utils/createMockRouter';
 
+const changePage = jest.fn();
+
 const setUp = (page: number, totalPages: number, router: Partial<NextRouter> = {}) => {
   const mockedRouter = mockNextRouter(router);
   const { rerender } = render(
-    <CardsControl currentPage={page} totalPages={totalPages} />
+    <CardsControl currentPage={page} totalPages={totalPages} changePage={changePage} />
   );
   const prevPaginationButton = screen.getByRole('button', {
     name: /go to previous page/i,
@@ -32,6 +34,7 @@ describe('CardsControl component', () => {
 
   beforeEach(() => {
     user = userEvent.setup();
+    jest.resetAllMocks();
   });
 
   test('should render the CardsControl component', () => {
@@ -49,23 +52,28 @@ describe('CardsControl component', () => {
   test('should be the active pagination page changed when the user click on the pagination page', async () => {
     const { rerender, maxPaginationPage } = setUp(1, 50);
     await user.click(maxPaginationPage);
-    rerender(<CardsControl currentPage={50} totalPages={50} />);
+    expect(changePage).toHaveBeenCalledTimes(1);
+    rerender(<CardsControl currentPage={50} totalPages={50} changePage={changePage} />);
     expect(screen.getByRole('button', { name: /page 50/i })).toHaveClass('Mui-selected');
     await user.click(screen.getByRole('button', { name: /go to page 46/i }));
-    rerender(<CardsControl currentPage={46} totalPages={50} />);
+    expect(changePage).toHaveBeenCalledTimes(2);
+    rerender(<CardsControl currentPage={46} totalPages={50} changePage={changePage} />);
     expect(screen.getByRole('button', { name: /page 46/i })).toHaveClass('Mui-selected');
   });
 
   test('should be prev/next pagination pages enabled or disabled when user click on the pagination page', async () => {
     const { rerender, prevPaginationButton, nextPaginationButton } = setUp(1, 50);
     await user.click(nextPaginationButton);
-    rerender(<CardsControl currentPage={2} totalPages={50} />);
+    expect(changePage).toHaveBeenCalledTimes(1);
+    rerender(<CardsControl currentPage={2} totalPages={50} changePage={changePage} />);
     expect(prevPaginationButton).toBeEnabled();
     await user.click(screen.getByRole('button', { name: /go to page 50/i }));
-    rerender(<CardsControl currentPage={50} totalPages={50} />);
+    expect(changePage).toHaveBeenCalledTimes(2);
+    rerender(<CardsControl currentPage={50} totalPages={50} changePage={changePage} />);
     expect(nextPaginationButton).toBeDisabled();
     await user.click(prevPaginationButton);
-    rerender(<CardsControl currentPage={49} totalPages={50} />);
+    expect(changePage).toHaveBeenCalledTimes(3);
+    rerender(<CardsControl currentPage={49} totalPages={50} changePage={changePage} />);
     expect(nextPaginationButton).toBeEnabled();
   });
 });
